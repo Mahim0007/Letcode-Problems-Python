61f9933f65a79a67f052ffab71ca15a839e902df
Mahim0007 <165550668+Mahim0007@users.noreply.github.com>
2025-12-07 12:03:38 +0600

    Time: 9 ms (60.43%) | Memory: 12.2 MB (98.67%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self,x):
+        if x < 0:
+            return False
+        s = str(x)
+        if s == s[::-1]:
+            return True
+        else:
+            return False
